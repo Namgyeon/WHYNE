3a0f54d7ccb0be95d2a1bbe1617342beb3835eea
서경수 <kssuh2956@gmail.com>
2025-02-06 09:05:06 +0900

    Feat: 공용 컴포넌트 Card-Flavor 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import KyungsuTest from "./test/KyungsuTest";
-
 export default function Home() {
   return (
     <div>
       <p className="text-black text-2xl-24px-bold"></p>
-      <KyungsuTest />
     </div>
   );
 }
