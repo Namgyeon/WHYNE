2ac87c9c1ed34366aeb86da73975da1a0950eeb7
서경수 <kssuh2956@gmail.com>
2025-02-09 09:19:58 +0900

    vercel test

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // 빌드 시 ESLint 오류 무시
-  },
+
 };
 
 export default nextConfig;
