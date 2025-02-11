2d50fc4e80f947e2e0dfd2fa1c704adb8381f878
Hyesun Kim <cocoabearhot@naver.com>
2025-02-11 16:35:35 +0900

    feat: 로그인, 회원가입 로그인시 홈으로 리다이렉됨

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,13 +1,19 @@
+"use client";
+
+import { AuthProvider } from "@/context/AuthProvider";
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <main className="flex justify-center items-center w-full px-[16px] md:px-0 h-screen bg-gray-100">
-      <section className="flex flex-col items-center gap-[24px] md:gap-[32px] w-full md:w-[496px] px-[20px] md:px-[48px] py-[56px] md:py-[64px] lg:py-[80px] bg-white border border-gray-300 rounded-[16px]">
-        {children}
-      </section>
-    </main>
+    <AuthProvider>
+      <main className="flex justify-center items-center w-full px-[16px] md:px-0 h-screen bg-gray-100">
+        <section className="flex flex-col items-center gap-[24px] md:gap-[32px] w-full md:w-[496px] px-[20px] md:px-[48px] py-[56px] md:py-[64px] lg:py-[80px] bg-white border border-gray-300 rounded-[16px]">
+          {children}
+        </section>
+      </main>
+    </AuthProvider>
   );
 }
