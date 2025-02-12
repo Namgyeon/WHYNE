55859744627ad281e2594cb828d5b93de9cf904b
Hyesun Kim <cocoabearhot@naver.com>
2025-02-12 16:44:07 +0900

    fix: oauthkakao/page.tsx 수정

--- src/app/(auth)/oauth/callback/KakaoOAuthWithSuspense.tsx
+++ src/app/(auth)/oauth/callback/KakaoOAuthWithSuspense.tsx
@@ -1,13 +1,12 @@
-// KakaoOAuthWithSuspense.tsx
 "use client"; // 클라이언트 전용
 
 import { Suspense } from "react";
-import KakaoOAuth from "./KakaoOAuth";
+import KakaoCallback from "./kakao/page";
 
 export default function KakaoOAuthWithSuspense() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <KakaoOAuth />
+      <KakaoCallback />
     </Suspense>
   );
 }
--- src/app/(auth)/oauth/callback/kakao/page.tsx
+++ src/app/(auth)/oauth/callback/kakao/page.tsx
@@ -1,19 +1,28 @@
-"use client";
+// /oauth/callback/kakao/page.tsx
+"use client"; // 클라이언트 전용으로 처리
 
-import { useEffect } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useSearchParams } from "next/navigation"; // URL 쿼리 파라미터 접근
 import { signInWithKakao } from "@/lib/api/kakaoAuth"; // 카카오 로그인 API 호출 함수
 import { AxiosError } from "axios";
 
-export default function KakaoCallback() {
+const KakaoCallback = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const code = searchParams.get("code"); // URL에서 'code' 파라미터 추출
+  const [code, setCode] = useState<string | null>(null);
 
   useEffect(() => {
-    console.log("URL에서 받은 code:", code); // 여기에 code 값을 출력
+    const fetchedCode = searchParams.get("code");
+    if (fetchedCode) {
+      setCode(fetchedCode);
+    } else {
+      console.error("code 파라미터가 없습니다.");
+      router.push("/signin");
+    }
+  }, [searchParams, router]);
 
+  useEffect(() => {
     if (code) {
       // 카카오 OAuth 인증 코드가 존재하면 서버에서 액세스 토큰을 얻어옴
       const authenticateWithKakao = async () => {
@@ -26,7 +35,7 @@ export default function KakaoCallback() {
         } catch (error) {
           if (error instanceof AxiosError) {
             console.error(
-              "!!!!!카카오 로그인 실패:",
+              "카카오 로그인 실패:",
               error.response?.data || error.message
             );
             // 실패 시 실패 페이지나 메세지 처리
@@ -39,9 +48,6 @@ export default function KakaoCallback() {
       };
 
       authenticateWithKakao();
-    } else {
-      console.error("code 파라미터가 없습니다."); // code가 없을 경우 오류 출력
-      router.push("/signin");
     }
   }, [code, router]);
 
@@ -49,5 +55,14 @@ export default function KakaoCallback() {
     <div className="flex justify-center items-center min-h-screen">
       <p>카카오 로그인 중...</p>
     </div>
+  );
+};
+
+// Suspense로 감싸서 클라이언트 전용 컴포넌트를 처리합니다.
+export default function KakaoCallbackPage() {
+  return (
+    <Suspense fallback={<div>로딩 중...</div>}>
+      <KakaoCallback />
+    </Suspense>
   );
 }
